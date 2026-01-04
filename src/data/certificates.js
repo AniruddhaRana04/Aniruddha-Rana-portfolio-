3a8bab0c54982af8afe1f2217304dd21c482dd62
AniruddhaRana04 <ranababuani@gmail.com>
2026-01-04 11:46:16 +0530

    4.1.2026

--- src/data/certificates.js
+++ src/data/certificates.js
@@ -1,24 +1,6 @@
 export const certificates = [
   {
     id: 'cert-1',
-    title: 'Madhyamik Examination',
-    issuer: 'West Bengal Board of Secondary Education',
-    date: '2020-07',
-    description: 'Madhyamik Examination is a significant academic milestone in West Bengal, India, marking the completion of secondary education. It is administered by the West Bengal Board of Secondary Education (WBBSE) and serves as a foundation for higher studies and career opportunities.',
-    image: '/certificates/CLASS10CERTIFICATE.jpg',
-    href: '/certificates/CLASS10CERTIFICATE.jpg'
-  },
-  {
-    id: 'cert-2',
-    title: 'Higher Secondary Examination',
-    issuer: 'West Bengal Board of Secondary Education',
-    date: '2022-06',
-    description: 'Higher Secondary Examination is a significant academic milestone in West Bengal, India, marking the completion of higher secondary education. It is administered by the West Bengal Board of Higher Secondary Education (WBBSE) and serves as a foundation for higher studies and career opportunities.',
-    image: '/certificates/CLASS12CERTIFICATE.jpg',
-    href: '/certificates/CLASS12CERTIFICATE.jpg'
-  },
-  {
-    id: 'cert-3',
     title: 'Complete 2025 Python Bootcamp: Learn Python fromScratch',
     issuer: 'www.codewithharry.com',
     date: '2025-12',
@@ -27,7 +9,7 @@ export const certificates = [
     href: '/certificates/python.jpeg'
   },
   {
-    id: 'cert-4',
+    id: 'cert-2',
     title: 'The Ultimate Job Ready Data Science Course',
     issuer: 'www.codewithharry.com',
     date: '2025-12',
