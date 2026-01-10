ef8363898e98b373b87a007417e01ff5c24693a9
AniruddhaRana04 <ranababuani@gmail.com>
2026-01-10 21:37:23 +0530

    Add Certificate

--- src/data/certificates.js
+++ src/data/certificates.js
@@ -16,5 +16,33 @@ export const certificates = [
     description: 'An in-depth Data Science course covering basics to advanced topics, including data analysis, machine learning, and more, designed for beginners and experienced programmers alike.',
     image: '/certificates/data-science.jpeg',
     href: '/certificates/data-science.jpeg'
+  },
+  {
+    id: 'cert-3',
+    title: 'Program in Professional Edge',
+    issuer: 'NIIT Foundation (Supported by Infosys Foundation)',
+    date: '2025-02',
+    description: 'Successfully completed the Program in Professional Edge conducted by NIIT Foundation from 30 January 2025 to 13 February 2025 and awarded an Excellent grade.',
+    image: '/certificates/niit.jpeg',
+    href: '/certificates/niit.jpeg'
+  },
+  {
+    id: 'cert-4',
+    title: 'Python For Absolute Beginners',
+    issuer: 'Midnapore College (Autonomous)',
+    date: '2023-12',
+    description: 'Successfully completed a 40-hour course on Python For Absolute Beginners with a consolidated grade of B+. The course was organized by the Centre of AI and Robotics, Department of BCA, Placement and Career Counselling Cell, Midnapore College (Autonomous).',
+    image: '/certificates/PythonMid.jpg',
+    href: '/certificates/PythonMid.jpg'
+  },
+  {
+    id: 'cert-5',
+    title: 'Virtual Internship Program in Web Development',
+    issuer: 'Bharat Intern',
+    date: '2023-10',
+    description: 'Successfully completed the Virtual Internship Program in Web Development at Bharat Intern as an active participant from 10 September 2023 to 10 October 2023. The internship is verified by Bharat Intern and recognized under the MSME initiative.',
+    image: '/certificates/webdev.jpeg',
+    href: '/certificates/webdev.jpeg'
   }
-]
+
+];
